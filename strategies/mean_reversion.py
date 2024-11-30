9aa4d3cc3d9a93600194027f78e8f7be682a1510
Joshua Lee <jlee0247@uni.sydney.edu.au>
2024-11-30 15:32:20 +1100

    added reversion class and prelim signal gen

--- strategies/mean_reversion.py
+++ strategies/mean_reversion.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from indicators import rsi, bollinger_bands
 
-
+# Use OHLCV data from ccxt as data input and JSON config file as input
+def mean_reversion_strat(data, config):
+    pass
+    
 
